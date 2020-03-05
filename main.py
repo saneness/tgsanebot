e3baa54fc378500d2316f04d661c0a3d1bf67d77
saneness <saneness@protonmail.com>
2020-03-05 04:59:31 -0500

    some updates

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import os
 import telegram
 import subprocess
 from telegram.ext import Updater, CommandHandler
@@ -34,9 +33,12 @@ class Utils:
 # Handlers
 def rrc(update, context):
     if update.message.from_user.username == 'saneness':
-        command = ' '.join(context.args)
-        stream = os.popen('{command}'.format(command=command))
-        text = '$ ' + '\n'.join([command, stream.read()])
+        if len(context.args) > 0:
+            command = ' '.join(context.args)
+            result = subprocess.check_output(context.args).decode('utf-8')
+            text = '$ ' + '\n'.join([command, result])
+        else:
+            text = 'Empty command. Nothing to do.'
     else:
         text = 'Permission denied.'
     context.bot.delete_message(chat_id=update.message.chat_id,
@@ -44,15 +46,23 @@ def rrc(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
 
 def start(update, context):
-    custom_keyboard = [['/start', '/help'], ['/system', '/service']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+    if len(context.args) > 0 and context.args[0] == 'keyboard':
+        custom_keyboard = [['/start', '/help'], ['/system', '/service']]
+        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+    else:
+        reply_markup = telegram.ReplyKeyboardRemove()
 
     text = 'I\'m a `TGSaneBot`! What do you want?'
 
     context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
 
 def help(update, context):
-    text = '/start - initialize me again\n/help - show this message\n/system - show system info\n/service - show service info'
+    text = '' \
+        '/start - initialize me again (add \'keyboard\' to enable keyboard buttons)\n' \
+        '/help - show all commands\n' \
+        '/system - show system info\n' \
+        '/service - show service info\n' \
+        '/rrc - run remote command (restricted access)'
     context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
 
 def system(update, context):
