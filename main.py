52002ba3f1aee18349fe98e4ebd121952f303f14
saneness <saneness@protonmail.com>
2020-11-17 17:44:59 -0500

    uploads

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import logging
@@ -5,6 +6,7 @@ import telegram
 import subprocess
 from telegram import bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from functools import wraps
 
 import psutil
 
@@ -13,19 +15,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 ADMIN_IDS = [
     430673891   # AZ
 ]
-DOWNLOAD_IDS = [
-    430673891,  # AZ
-    173715757,  # UV
-    769506446,  # DZ
-    430673891,  # AA
-    279002221   # DI
-]
+FAMILY_IDS = {
+    430673891: 'saneness',  # AZ
+    173715757: 'saaph'  # UV
+    # 769506446  # DZ
+    # 430673891,  # AA
+    # 279002221   # DI
+}
 MAX_DISK_USAGE = 90
 
 class Utils:
+    @staticmethod
     def pre(text):
         return '```\n' + text + '\n```'
 
+    @staticmethod
     def table(array, header=None, widths=None):
         n = len(array[0])
         if header:
@@ -37,45 +41,54 @@ class Utils:
             table += ['|'.join([f' {item[i]:{widths[i]}s}' for i in range(n)])]
         return '\n'.join(table)
 
+
+def admin(func):
+    @wraps(func)
+    def wrapper(update, context, *args, **kwargs):
+        user_id = update.message.from_user.id
+        if user_id not in ADMIN_IDS:
+            text = 'Permission denied.'
+            context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapper
+
+def family(func):
+    @wraps(func)
+    def wrapper(update, context, *args, **kwargs):
+        user_id = update.message.from_user.id
+        if user_id not in FAMILY_IDS:
+            text = 'Permission denied.'
+            context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapper
+
+@admin
 def rrc(update, context):
-    if update.message.from_user.id in ADMIN_IDS:
-        if len(context.args) > 0:
-            command = ' '.join(context.args)
-            try:
-                result = subprocess.check_output(context.args, stderr=subprocess.STDOUT).decode('utf-8')
-                text = '$ ' + '\n'.join([command, result])
-            except subprocess.CalledProcessError as err:
-                text = '$ ' + '\n'.join([command, err.output.decode('utf-8')])
-            except Exception as err:
-                text = '$ ' + '\n'.join([command, str(err)])
-            except:
-                text = '$ ' + '\n'.join([command, 'Something went wrong.'])
-        else:
-            text = 'Empty command. Nothing to do.'
+    if len(context.args) > 0:
+        command = ' '.join(context.args)
+        try:
+            result = subprocess.check_output(context.args, stderr=subprocess.STDOUT).decode('utf-8')
+            text = '$ ' + '\n'.join([command, result])
+        except subprocess.CalledProcessError as err:
+            text = '$ ' + '\n'.join([command, err.output.decode('utf-8')])
+        except Exception as err:
+            text = '$ ' + '\n'.join([command, str(err)])
+        except:
+            text = '$ ' + '\n'.join([command, 'Something went wrong.'])
     else:
-        text = 'Permission denied.'
+        text = 'Empty command. Nothing to do.'
     context.bot.delete_message(chat_id=update.message.chat_id,
                                message_id=update.message.message_id)
     context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
 
 def start(update, context):
-    if len(context.args) > 0 and context.args[0] == 'keyboard':
-        custom_keyboard = [['/start', '/help'], ['/system', '/service']]
-        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
-    else:
-        reply_markup = telegram.ReplyKeyboardRemove()
+    reply_markup = telegram.ReplyKeyboardRemove()
     text = 'I\'m a `TGSaneBot`! What do you want?'
     context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN, reply_markup=reply_markup)
 
-def help(update, context):
-    text = '' \
-        '/start - initialize me again (add \'keyboard\' to enable keyboard buttons)\n' \
-        '/help - show all commands\n' \
-        '/system - show system info\n' \
-        '/service - show service info\n' \
-        '/rrc - run remote command (restricted access)'
-    context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
-
+@admin
 def system(update, context):
     memory = psutil.virtual_memory()
     memory_used = memory.used / 1024 / 1024
@@ -90,12 +103,10 @@ def system(update, context):
     text = Utils.table(system_status, header=['system', 'status'], widths=[8, 18])
     context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
 
+@admin
 def service(update, context):
     def get_status(service):
         return str(subprocess.call(['systemctl', 'is-active', '--quiet', service]))
-
-    def row(data):
-        return '|'.join(data) + '\n'
 
     status = {'0': 'running', '1': 'error', '3': 'stopped'}
     service_list = ['nginx', 'apache2', 'mtproxy-faketls', 'openvpn', 'tgsanebot']
@@ -103,27 +114,29 @@ def service(update, context):
     text = Utils.table(service_status, header=['service', 'status'], widths=[16, 10])
     context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
 
-def download_photo(update, context):
-    if update.message.from_user.id in DOWNLOAD_IDS:
-        disk = psutil.disk_usage('/')
-        if 100.0 * disk.used / disk.total < MAX_DISK_USAGE:
-            photo_id = update.message.photo[-1].file_id
-            photo_data = context.bot.get_file(photo_id)
-            photo_src = '/root/downloads/tgsanebot/photo/' + photo_id + '.jpg'
-            photo_data.download(photo_src)
-            text = 'Download complete!'
-            context.bot.delete_message(chat_id=update.message.chat_id,
-                                       message_id=update.message.message_id)
-            reply = context.bot.send_message(chat_id=update.message.chat_id, text=text)
-            time.sleep(0.5)
-            context.bot.delete_message(chat_id=update.message.chat_id,
-                                       message_id=reply.message_id)
-        else:
-            text = f'Disk usage is over {MAX_DISK_USAGE}%. Please ask the administrator to free some space before trying again.'
-            context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN) 
+@family
+def upload(update, context):
+    disk = psutil.disk_usage('/')
+    if 100.0 * (disk.used + update.message.effective_attachment.file_size) / disk.total < MAX_DISK_USAGE:
+        chat_id = update.message.chat_id
+        username = FAMILY_IDS[chat_id]
+        message_id = update.message.message_id
+        file_id = update.message.effective_attachment.file_id
+        file_name = update.message.effective_attachment.file_name.replace(' ', '_')
+        file_data = context.bot.get_file(file_id)
+        if not os.path.exists(f'/var/www/saaph.online/uploads/{username}//'):
+            os.makedirs(f'/var/www/saaph.online/uploads/{username}/')
+        file_path = f'/var/www/saaph.online/uploads/{username}/{file_name}'
+        file_data.download(file_path)
+        text = 'Upload complete!\n' \
+            f'Your file location: https://saaph.online/uploads/{username}/{file_name}'
+        context.bot.delete_message(chat_id=chat_id,
+                                    message_id=message_id)
+        context.bot.send_message(chat_id=chat_id, text=text)
     else:
-        text = 'Permission denied.'
+        text = f'Disk usage is over {MAX_DISK_USAGE}%. Please ask the administrator to free some space before trying again.'
         context.bot.send_message(chat_id=update.message.chat_id, text=Utils.pre(text), parse_mode=telegram.ParseMode.MARKDOWN)
+
 
 def main():
     with open('.token') as f:
@@ -133,10 +146,9 @@ def main():
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('rrc', rrc))
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', help))
     dispatcher.add_handler(CommandHandler('system', system))
     dispatcher.add_handler(CommandHandler('service', service))
-    dispatcher.add_handler(MessageHandler(Filters.photo, download_photo));
+    dispatcher.add_handler(MessageHandler(Filters.document, upload));
     updater.start_polling()
 
 if __name__ == '__main__':
